62d8c2b0c5fa4865a2d6d19fd0989bab3282a734
Victor25Ortiz <vortiz25@stu.southtexascollege.edu>
2024-01-25 15:54:20 -0600

    todo list partial working

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script setup>
 import {ref} from 'vue'
-
+const todos = ref([])
 const name = ref('')
+const input_content = ref('')
+
+const addTodo = () => {
+  if(input_content.value.trim() === '') {
+    return
+  }
+
+  todos.value.push({
+    content: input_content.value,
+  })
+
+  input_content.value = ''
+  //console.log(todos)
+
+}
 </script>
 
 <template>
@@ -14,9 +29,16 @@ const name = ref('')
     </section>
 
     <section class="create-todo">
+      <h3>CREATE YOUR DAILY TO DO LIST</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What do you need to get done today?</h4>
+        <input type="text" placeholder="e.g, Pick up groceries" v-model="input_content"/>
+        <!-- {{ input_content }} -->
+      </form>
     </section>
 
     <section class="todo-list">
+
     </section>
   </main>
 </template>
